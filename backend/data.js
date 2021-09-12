519373a96ce78aa45ecc0c2afba68b69a51704fc
Man <voongngocman@gmail.com>
2021-09-12 21:37:22 +0700

    initial commit

---
+++ backend/data.js
@@ -0,0 +1,205 @@
+const data = {
+    products : [
+
+        {
+            _id:'1',
+            title:"Vòng tay giả cỏ",
+            price: 30000,
+            image01: "../image/products/p1_img1.jpg",
+            image02: "../image/products/p1_img2.jpg",
+            categorySlug: "vong-tay",
+            colors: ["green","brow"],
+            slug: "vong-tay-gia-co",
+            size: ["15","16","17","18"]
+        },
+        {
+            _id:'2',
+            title:"Vòng trơn xanh trắng",
+            price: 30000,
+            image01: "..frontend/src/assets/image/products/p2_img1.jpg",
+            image02: "..frontend/src/assets/image/products/p2_img2.jpg",
+            categorySlug: "vong-tay",
+            colors: ["green","white"],
+            slug: "vong-tay-xanh-trang",
+            size: ["15","16","17","18"]
+        },
+        {
+            _id:'3',
+            title:"Vòng trơn đỏ xanh",
+            price: 30000,
+            image01: "..frontend/src/assets/image/products/p3_img1.jpg",
+            image02: "..frontend/src/assets/image/products/p3_img2.jpg",
+            categorySlug: "vong-tay",
+            colors: ["red","blue"],
+            slug: "vong-tay-do-xanh",
+            size: ["15","16","17","18"]
+        },
+        {
+            _id:'4',
+            title:"Vòng mạ vàng phong cách",
+            price: 75000,
+            image01: "..frontend/src/assets/image/products/p4_img1.jpg",
+            image02: "..frontend/src/assets/image/products/p4_img1.jpg",
+            categorySlug: "vong-tay",
+            colors: ["gold","yellow"],
+            slug: "vong-tay-ma-vang",
+            size: ["15","16","17","18"]
+        },
+        {
+            _id:'5',
+            title:"Vòng số 5",
+            price: 30000,
+            image01: "..frontend/src/assets/image/products/p5_img1.jpg",
+            image02: "..frontend/src/assets/image/products/p5_img2.jpg",
+            categorySlug: "vong-tay",
+            colors: ["green","brow"],
+            slug: "vong-tay-gia-co",
+            size: ["15","16","17","18"]
+        },
+        {
+            _id:'6',
+            title:"Vòng số 6",
+            price: 30000,
+            image01: "..frontend/src/assets/image/products/p6_img1.jpg",
+            image02: "..frontend/src/assets/image/products/p6_img2.jpg",
+            categorySlug: "vong-tay",
+            colors: ["green","brow"],
+            slug: "vong-tay-gia-co",
+            size: ["15","16","17","18"]
+        },
+        {
+            _id:'7',
+            title:"Vòng số 7",
+            price: 30000,
+            image01: "..frontend/src/assets/image/products/p7_img1.jpg",
+            image02: "..frontend/src/assets/image/products/p7_img2.jpg",
+            categorySlug: "vong-tay",
+            colors: ["green","brow"],
+            slug: "vong-tay-gia-co",
+            size: ["15","16","17","18"]
+        },
+        {
+            _id:'8',
+            title:"Vòng số 8",
+            price: 30000,
+            image01: "..frontend/src/assets/image/products/p8_img1.jpg",
+            image02: "..frontend/src/assets/image/products/p8_img2.jpg",
+            categorySlug: "vong-tay",
+            colors: ["green","brow"],
+            slug: "vong-tay-gia-co",
+            size: ["15","16","17","18"]
+        },
+        {
+            _id:'9',
+            title:"Vòng số 9",
+            price: 30000,
+            image01: "..frontend/src/assets/image/products/p9_img1.jpg",
+            image02: "..frontend/src/assets/image/products/p9_img2.jpg",
+            categorySlug: "vong-tay",
+            colors: ["green","brow"],
+            slug: "vong-tay-gia-co",
+            size: ["15","16","17","18"]
+        },
+        {
+            _id:'10',
+            title:"Vòng số 10",
+            price: 30000,
+            image01: "..frontend/src/assets/image/products/p10_img1.jpg",
+            image02: "..frontend/src/assets/image/products/p10_img2.jpg",
+            categorySlug: "vong-tay",
+            colors: ["green","brow"],
+            slug: "vong-tay-gia-co",
+            size: ["15","16","17","18"]
+        },
+        {
+            _id:'11',
+            title:"Vòng số 11",
+            price: 30000,
+            image01: "..frontend/src/assets/image/products/p12_img1.jpg",
+            image02: "..frontend/src/assets/image/products/p12_img2.jpg",
+            categorySlug: "vong-tay",
+            colors: ["green","brow"],
+            slug: "vong-tay-gia-co",
+            size: ["15","16","17","18"]
+        },
+        {
+            _id:'12',
+            title:"Vòng số 12",
+            price: 30000,
+            image01: "..frontend/src/assets/image/products/p12_img1.jpg",
+            image02: "..frontend/src/assets/image/products/p12_img2.jpg",
+            categorySlug: "vong-tay",
+            colors: ["green","brow"],
+            slug: "vong-tay-gia-co",
+            size: ["15","16","17","18"]
+        },
+        {
+            _id:'13',
+            title:"Vòng số 13",
+            price: 30000,
+            image01: "..frontend/src/assets/image/products/p11_img1.jpg",
+            image02: "..frontend/src/assets/image/products/p11_img2.jpg",
+            categorySlug: "vong-tay",
+            colors: ["green","brow"],
+            slug: "vong-tay-gia-co",
+            size: ["15","16","17","18"]
+        },
+        { 
+            _id:'14',
+            title:"Vòng số 14",
+            price: 30000,
+            image01: "..frontend/src/assets/image/products/p12_img1.jpg",
+            image02: "..frontend/src/assets/image/products/p12_img2.jpg",
+            categorySlug: "vong-tay",
+            colors: ["green","brow"],
+            slug: "vong-tay-gia-co",
+            size: ["15","16","17","18"]
+        },
+        {
+            _id:'15',
+            title:"Vòng số 15",
+            price: 30000,
+            image01: "..frontend/src/assets/image/products/p12_img1.jpg",
+            image02: "..frontend/src/assets/image/products/p12_img2.jpg",
+            categorySlug: "vong-tay",
+            colors: ["green","brow"],
+            slug: "vong-tay-gia-co",
+            size: ["15","16","17","18"]
+        },
+        {
+            _id:'16',
+            title:"Vòng số 16",
+            price: 30000,
+            image01: "..frontend/src/assets/image/products/p13_img1.jpg",
+            image02: "..frontend/src/assets/image/products/p13_img2.jpg",
+            categorySlug: "vong-tay",
+            colors: ["green","brow"],
+            slug: "vong-tay-gia-co",
+            size: ["15","16","17","18"]
+        },
+        {
+            _id:'17',
+            title:"Vòng số 17",
+            price: 30000,
+            image01: "..frontend/src/assets/image/products/p14_img1.jpg",
+            image02: "..frontend/src/assets/image/products/p14_img2.jpg",
+            categorySlug: "vong-tay",
+            colors: ["green","brow"],
+            slug: "vong-tay-gia-co",
+            size: ["15","16","17","18"]
+        },
+        {
+            _id:'18',
+            title:"Vòng số 18",
+            price: 30000,
+            image01: "..frontend/src/assets/image/products/p15_img1.jpg",
+            image02: "..frontend/src/assets/image/products/p15_img2.jpg",
+            categorySlug: "vong-tay",
+            colors: ["green","brow"],
+            slug: "vong-tay-gia-co",
+            size: ["15","16","17","18"]
+        },
+    
+    ],
+};
+export default data
